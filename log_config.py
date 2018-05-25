1463ae452a5f61fb0dd4ef5405b3821d2e78d94c
Водопьянов Антон Сергеевич <Anton.Vodopyanov@moex.com>
2018-05-25 17:28:55 +0300

    logger done

--- log_config.py
+++ log_config.py
@@ -1,71 +1,39 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import sys
 
-def test_func1():
-    log.debug('This message is from function')
-    log.info('Info message')
-    log.critical('CRITICAAAAL!!!')
 
-def test_func2():
-    pass
+def log(func):
+    def callf(*args, **kwargs):
+        # Containes function from which was called
+        call_log = sys._getframe(1).f_code.co_name
+        app_log.debug(f'Function {func.__name__}: args:{args}, kwargs:{kwargs}, called by: {call_log}')
+        return func(*args, **kwargs)
+    return callf
 
 
-log = logging.getLogger(__name__)
+app_log = logging.getLogger('app')
 
 # Setting logging level
-log.setLevel(logging.DEBUG)
+app_log.setLevel(logging.DEBUG)
 
 # Set log message format
 # <datetime> <level> <module name> <function call name> <message>
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
 
-# Adding handler for writing log to file
-log_handler = logging.FileHandler('app.log')
+# Adding handler for writing log to file with each day file rotating
+if __name__ == 'server':
+    log_handler = TimedRotatingFileHandler(f'app_server.log', backupCount=7, when='D', interval=1)
+else:
+    log_handler = logging.FileHandler(f'app_{__name__}.log')
 log_handler.setLevel(logging.DEBUG)
 log_handler.setFormatter(formatter)
 
+
 # Adding handler for writing log to console
 stream_handler = logging.StreamHandler(sys.stderr)
-stream_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.CRITICAL)
 stream_handler.setFormatter(formatter)
 
-log.addHandler(log_handler)
-log.addHandler(stream_handler)
-
-
-
-test_func1()
-
-
-
-
-
-
-
-# Основное задание:
-# ● Реализовать логгирование с использованием модуля logging:
-# ○ Реализовать декоратор @log, фиксирующий обращение к декорируемой функции:
-# сохраняет имя функции и её аргументы.
-# ○ Настройку логгера выполнить в отдельном модуле log_config.py:
-# ● Создание именованного логгера.
-# ● Сообщения лога должны иметь следующий формат:
-# "<дата-время> <уровень_важности> <имя_модуля> <имя_функции> <сообщение>"
-# ● Журналирование должно производиться в лог-файл.
-# ● На стороне сервера необходимо настроить ежедневную ротацию лог-файлов
-# ● Реализовать обработку нескольких клиентов на сервере с использованием
-# функции select таким образом, что клиенты общаются в "общем чате", т.е.
-# каждое сообщение каждого клиента отправляется всем клиентам,
-# подключенным к серверу.
-# ● Реализовать функции отправки/приёма данных на стороне клиента. Для
-# упрощения разработки приложения на данном этапе пусть клиентское
-# приложение будет либо только принимать, либо только отправлять сообщения в
-# общий чат:
-# ● запуск скрипта клиента должен осуществляться с параметром командной
-# строки: -r (чтение чата) или -w (передача сообщений в чат).
-# ● Для всех функций необходимо написать тесты.
-
-# В декораторе @log реализовать фиксацию функции, из которой была вызвана декорированная
-# функция. Т.е
-
-# То в логе должна быть отражена информация:
-# "<дата-время> Функция func_z() вызвана из функции main"
+app_log.addHandler(log_handler)
+app_log.addHandler(stream_handler)
