3f343ba71d371f82ff89216edc7bde2ac521db91
Anton Vodopyanov <vodopyanovas@ya.ru>
2018-05-26 14:25:43 +0300

    multiple clients starter added

---
+++ start_multiple_clients.py
@@ -0,0 +1,22 @@
+from subprocess import Popen, CREATE_NEW_CONSOLE
+
+# Client processes list
+p_list = []
+
+while True:
+    user = input('Start multiple clients (s) / Close clients (x) / Quit (q)' + '\n' + '>> ')
+
+    if user == 'q':
+        break
+    elif user == 's':
+        n = input('How many clients to start: ')
+        for _ in range(int(n)):
+            # Start 5 processes in separate console window
+            p_list.append(Popen('py client.py', creationflags=CREATE_NEW_CONSOLE))
+
+        print(f'{n} clients started')
+
+    elif user == 'x':
+        for p in p_list:
+            p.kill()
+        p_list.clear()
