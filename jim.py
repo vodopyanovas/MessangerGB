df8a3c936f7de206fab64857e514fe5bbd4b3c05
Водопьянов Антон Сергеевич <Anton.Vodopyanov@moex.com>
2018-06-04 16:43:31 +0300

    refactored to class architecture

--- jim.py
+++ jim.py
@@ -2,57 +2,117 @@
 
 from utils import timestamp
 
-RESPONSE_CODE = {
+
+class JIMMessage:
+
+    def presence(self, account, status=''):
+        msg = {
+            "action": "presence",
+            "time": timestamp(),
+            "type": "status" ,
+            "user": {
+                "account_name": account,
+                "status": status
+            }
+        }
+        return msg
+
+    def authenticate(self, account, password):
+        msg = {
+            "action": "authenticate",
+            "time": timestamp(),
+            "user": {
+                "account_name": account,
+                "password": password
+            }
+        }
+        return msg
+
+    def message(self, msg_to, msg_from, message):
+        msg = {
+            "action": "msg",
+            "time": timestamp(),
+            "to": msg_to,
+            "from": msg_from,
+            "encoding": "utf-8",
+            "message": message
+        }
+        return msg
+
+    def group_msg(self, msg_to, msg_from, message):
+        msg = {
+            "action": "msg",
+            "time": timestamp(),
+            "to": '#' + msg_to,
+            "from": msg_from,
+            "encoding": "utf-8",
+            "message": message
+        }
+        return msg
+
+    def join_chat(self, room_name):
+        msg = {
+            "action": "join",
+            "time": timestamp(),
+            "room": '#' + room_name
+        }
+        return msg
+
+    def leave_chat(self, room_name):
+        msg = {
+            "action": "leave",
+            "time": timestamp(),
+            "room": '#' + room_name
+        }
+        return msg
+
+class JIMResponse:
+
+    RESPONSE_CODE = {
     100: 'NOTIFICATION',
     101: 'IMPORTANT NOTIFICATION',
     200: 'OK',
     201: 'OBJECT CREATED',
     202: 'ACCEPTED'
-}
-
-ERROR_CODE = {
-    400: 'ERROR: WRONG REQUEST',
-    401: 'ERROR: NOT AUTHORISED',
-    402: 'ERROR: BAD CREDENTIALS',
-    403: 'ERROR: FORBIDDEN',
-    404: 'ERROR: NOT FOUND',
-    409: 'ERROR: LOGIN CONFLICT',
-    410: 'ERROR: USER OFFLINE',
-    500: 'SERVER ERROR'
-}
-
-# presense message
-def presence(account, status=''):
-    msg = {
-        "action": "presence",
-        "time": timestamp(),
-        "type": "status" ,
-        "user": {
-            "account_name": account,
-            "status": status
-        }
     }
-    return msg
 
-# server response
-def response(code):
-    if code < 400:
-        msg = {
-            'response': code,
-            'time': timestamp(),
-            'alert': RESPONSE_CODE[code],
-            'error' : ''
-        }
-    else:
-        msg = {
-            'response': code,
-            'time': timestamp(),
-            'alert': '',
-            'error' : ERROR_CODE[code]
-        }
-    return msg
+    ERROR_CODE = {
+        400: 'ERROR: WRONG REQUEST',
+        401: 'ERROR: NOT AUTHORISED',
+        402: 'ERROR: BAD CREDENTIALS',
+        403: 'ERROR: FORBIDDEN',
+        404: 'ERROR: NOT FOUND',
+        409: 'ERROR: LOGIN CONFLICT',
+        410: 'ERROR: USER OFFLINE',
+        500: 'SERVER ERROR'
+    }
+
+    def response(self, code):
+        if code < 400:
+            msg = {
+                'response': code,
+                'time': timestamp(),
+                'alert': self.RESPONSE_CODE[code]
+            }
+        else:
+            msg = {
+                'response': code,
+                'time': timestamp(),
+                'error' : self.ERROR_CODE[code]
+            }
+        return msg
+
+# message = JIMMessage()
+# response = JIMResponse()
 
 
+# print(message.presence('anton', status='online'))
+# print(message.authenticate('anton', '12345'))
+# print(message.message('timur','anton', 'Hello World!'))
+# print(message.group_msg('group chat','anton', 'Hello World!'))
+# print(message.join_chat('group chat'))
+# print(message.leave_chat('group chat'))
 
 
-
+# print(response.response(200))
+# print(response.response(400))
--- server.py
+++ server.py
@@ -2,54 +2,101 @@
 # Messanger server
 from socket import socket, AF_INET, SOCK_STREAM
 import logging
-import sys
 import select
 
 from utils import timestamp, encode_json, decode_json, arg_parser
-from jim import response
+from jim import JIMResponse
 import log_config
 from log_config import log
 
 
 app_log = logging.getLogger('app')
 
+response = JIMResponse()
 
-@log
-def start_server(address):
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.bind(address)
-    sock.listen(5)
-    sock.settimeout(0.2)
-    return sock
 
-def srv_loop():
-    address = (HOST, PORT)
-    clients = []
-    sock = start_server(address)
+class JIMServer:
 
-    while True:
+    def __init__(self):
+        self.clients = []
+
+    def start_server(self):
+        address = (HOST, PORT)
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.bind(address)
+        sock.listen(5)
+        sock.settimeout(0.2)
+        return sock
+
+    def get_client(self, sock):
         try:
             client, address = sock.accept()
         except OSError as e:
             pass
         else:
             print(f'Connected with {str(address)}')
-            clients.append(client)
+            self.clients.append(client)
         finally:
             w = []
             try:
-                r, w, e = select.select([], clients, [], 0)
-            except Exception as e:
+                r, w, e = select.select([], self.clients, [], 0)
+            except Exception as ex:
                 pass
+        return w
 
-        for s_client in w:
-            srv_response = encode_json(response(200))
+    def parse_msg(self, data):
+        print(data)
+        action = data['action']
+        if action == 'authenticate':
+            print('Checking auth params...')
+            check = 1
+            if check:
+                return 202, action
+            else:
+                return 402, action
+        elif action == 'presence':
+            # print('Presense message!')
+            return 200, action
+        elif action == 'msg':
+            print(f'Message to: {data["to"]}')
+            return 200, action, data["to"]
+        elif action == 'join':
+            # print('Join')
+            return 200, action
+        elif action == 'leave':
+            # print('Leave')
+            return 200, action
+        elif action == 'quit':
+            # print('Quit')
+            return 200, action
+
+
+    def get_data(self, client_socket):
+        for s_client in client_socket:
             try:
                 data = s_client.recv(1024)
-                print(decode_json(data))
-                s_client.send(srv_response)
+                decoded_data = decode_json(data)
+                parsed_data = self.parse_msg(decoded_data)
+                send_response = encode_json(response.response(parsed_data[0]))
+
+                # print(parsed_data[1])
+                s_client.send(send_response)
             except:
-                clients.remove(s_client)
+                self.clients.remove(s_client)
+
+    def server_loop(self):
+        sock = self.start_server()
+
+        while True:
+            w = self.get_client(sock)
+            self.get_data(w)
+
+    def probe(self):
+        msg = {
+            "action": "probe",
+            "time": timestamp()
+        }
+        return msg
 
 
 if __name__ == '__main__':
@@ -67,9 +114,9 @@ if __name__ == '__main__':
         PORT = 7777
 
     print('Server started')
-    srv_loop()
 
-
+    server = JIMServer()
+    server.server_loop()
 
 
 # имеет параметры командной строки:
